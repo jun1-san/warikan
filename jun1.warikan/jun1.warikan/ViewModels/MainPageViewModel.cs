c7649cde978ddc19fbe5e962f247159ffaacf2b7
jun1_san <stgoldensnake@gmail.com>
2019-12-30 12:33:35 +0900

    新規作成。ビルドおよび実機デバッグ可能な状態。

---
+++ jun1.warikan/jun1.warikan/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace jun1.warikan.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        public MainPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            Title = "Main Page";
+        }
+    }
+}
